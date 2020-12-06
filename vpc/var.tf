55ea46e958ffff3edef25720b6df36b69eabef0c
Karthik Bangalore Venkatakrishna <mk@Karthiks-MacBook-Pro.local>
2020-12-06 14:54:37 +0000

    corrected vpc moduel

--- vpc/var.tf
+++ vpc/var.tf
@@ -1,30 +1,75 @@
 variable "cidr" {
-    description = "pass cidr block "
-    type = string
-}
-
-variable "dns_support" {
-    description = " pass the value to enable dns_support"
-    default = true
-}
-
-variable "tags" {
-    description = " tags for vpc"
-    type = map(string)
-    default = {}
-}
-
-variable "subnet_cidr" {
-     description = "pass the address range"
-     type = string
-}     
-
-variable "service_name" {
-  description = " service name for resource"
+  description = "cidr block for vpc "
   type = string
 }
 
-variable "env_name" {
-  description = " environment name for resource"
-  type = string
+variable "instance_tenancy" {
+  description = "Teanancy value"
+  default = "shared"
+}
+
+variable "enable_dns_hostnames" {
+  description = "dns hostname for vpc"
+  default = "true"
+  
+}
+
+variable "enable_dns_support" {
+  type = bool
+  default = "true"
+  
+}
+
+variable "enable_classiclink" {
+  type = bool
+  default = "false"
+  
+}
+
+variable "enable_classiclink_dns_support" {
+  type = bool
+  default = "false"
+  
+}
+
+variable "tags_vpc" {
+  type = map (string)
+  default = {
+    
+  }
+
+  
+}
+
+variable "create_vpc" {
+  type = bool
+  default = true
+  
+}
+
+
+variable "create_gateway" {
+  type = bool
+  default = true
+  
+}
+
+variable "public_subnets" {
+  type = list (string)
+  default = []
+
+  
+}
+
+variable "igw_tags" {
+  type = map (string)
+  default = {
+
+  }
+
+  
+}
+
+variable "create_egress_only_igw" {
+  
 }
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,7 +1,96 @@
 resource "aws_vpc" "this" {
-  cidr_block       = var.cidr
-  instance_tenancy = "default"
-  enable_dns_support = var.dns_support
-  tags = var.tags
+  count = var.create_vpc ? 1 : 0
 
+  cidr_block                       = var.cidr
+  instance_tenancy                 = var.instance_tenancy
+  enable_dns_hostnames             = var.enable_dns_hostnames
+  enable_dns_support               = var.enable_dns_support
+  enable_classiclink               = var.enable_classiclink
+  enable_classiclink_dns_support   = var.enable_classiclink_dns_support
+ 
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags,
+    var.vpc_tags,
+  )
 }
+
+
+####internetgateway
+resource "aws_internet_gateway" "this" {
+  count = var.create_vpc && var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags,
+    var.igw_tags,
+  )
+}
+
+
+
+# ################
+# # Publiс routes
+# ################
+# resource "aws_route_table" "public" {
+#   count = var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0
+
+#   vpc_id = local.vpc_id
+
+#   tags = merge(
+#     {
+#       "Name" = format("%s-${var.public_subnet_suffix}", var.name)
+#     },
+#     var.tags,
+#     var.public_route_table_tags,
+#   )
+# }
+
+# resource "aws_route" "public_internet_gateway" {
+#   count = var.create_vpc && var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
+
+#   route_table_id         = aws_route_table.public[0].id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.this[0].id
+
+#   timeouts {
+#     create = "5m"
+#   }
+# }
+
+# resource "aws_route" "public_internet_gateway_ipv6" {
+#   count = var.create_vpc && var.create_igw && var.enable_ipv6 && length(var.public_subnets) > 0 ? 1 : 0
+
+#   route_table_id              = aws_route_table.public[0].id
+#   destination_ipv6_cidr_block = "::/0"
+#   gateway_id                  = aws_internet_gateway.this[0].id
+# }
+
+# #################
+# # Private routes
+# # There are as many routing tables as the number of NAT gateways
+# #################
+# resource "aws_route_table" "private" {
+#   count = var.create_vpc && local.max_subnet_length > 0 ? local.nat_gateway_count : 0
+
+#   vpc_id = local.vpc_id
+
+#   tags = merge(
+#     {
+#       "Name" = var.single_nat_gateway ? "${var.name}-${var.private_subnet_suffix}" : format(
+#         "%s-${var.private_subnet_suffix}-%s",
+#         var.name,
+#         element(var.azs, count.index),
+#       )
+#     },
+#     var.tags,
+#     var.private_route_table_tags,
+#   )
+# }
